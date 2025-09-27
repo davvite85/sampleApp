bb3d28f825ff00e5c7f4c6ce30d7fe41497cfa21
davvite85 <78754968+davvite85@users.noreply.github.com>
2025-09-27 07:40:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ def home():
     return """
     <html>
         <head><title>Hello World ! Good Morning !</title></head>
-        <body style="background-color: pink; color: white; text-align: center; padding-top: 200px;">
-            <h1>Hello World</h1>
+        <body style="background-color: black; color: white; text-align: center; padding-top: 200px;">
+            <h1>Good Morning!</h1>
         </body>
     </html>
     """
